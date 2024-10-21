ad04cc27d0891c9f290774105b8018f94eac21ca
deng451e <deng451e@gmail.com>
2024-10-21 11:55:29 -0500

    c3

--- beyond/KVcache_manager.py
+++ beyond/KVcache_manager.py
@@ -9,16 +9,16 @@ import time
 class KVCache_manager_:
     def __init__(
         self,
+        copy_stream=None,
         start_size=40,
         recent_size=1024,
         block_size=100,
-        k_seq_dim=1,
-        v_seq_dim=1,
+        k_seq_dim=2,
+        v_seq_dim=2,
         head_dim=128,
         num_heads=32,
         num_layers=32,
         gpu_cache_max=2000,
-        batched=True,
         gpu_cache_device="cpu",
     ):
         print(f"GPU attn device:{gpu_cache_device}")
@@ -30,24 +30,27 @@ class KVCache_manager_:
         self.v_seq_dim = v_seq_dim
         self.k_slice = DIM_TO_SLICE[k_seq_dim]
         self.v_slice = DIM_TO_SLICE[v_seq_dim]
-        self.batched = batched
         self.num_layers = num_layers
-        # k_gpu,v_gpu,k_cpu,v_cpu
+        self.copy_stream  =  copy_stream
+
+        #( k_gpu,v_gpu,k_cpu,v_cpu)
         self.kv_cache = [[None,None,None,None] for _ in range(num_layers)] 
+        
         # statics  for tracking gpu kv blocks   
-        self.start_sizes  = [start_size  for _ in range(num_layers)]  
-        self.recent_sizes = [recent_size  for _ in range(num_layers)] 
+        self.start_sizes   = [start_size  for _ in range(num_layers)]  
+        self.recent_sizes  = [recent_size  for _ in range(num_layers)] 
         self.gpu_cache_max = gpu_cache_max
         self.blk_size  = block_size
-        self.gpu_cache_device = gpu_cache_device 
-        # statics  for tracking cpu kv blocks   
-        self.cpu_kv_flags = [False for _ in range(num_layers)]   
+        self.gpu_cache_device = gpu_cache_device # device for offload gpu attn KV cache
+        
+        # statics  for tracking cpu kv blocks
+        self.cpu_kv_flags  = [False for _ in range(num_layers)]   
+        self.cpu_attn_size = [1000 for _ in range(num_layers)] 
+        self.blk_num       = [0 for _ in range(num_layers)] 
         self.blk_tracker = None 
-        self.blk_num   = [0 for _ in range(num_layers)] 
         self.blk_min_max = None 
          
-        self.copy_steam  =  torch.cuda.Stream()
-
+         
     
 
     def __call__(self,idx):
@@ -68,28 +71,27 @@ class KVCache_manager_:
 
     def add_kv_cache_by_layer(self, idx , kv_cache_2add):
          
-        
-         
+        k2add, v2add = kv_cache_2add
+        if k2add is None: return  
 
         k_gpu,v_gpu,k_cpu,v_cpu = self.kv_cache[idx]
-        k2add, v2add = kv_cache_2add
-        if k2add is None: return 
+        
         add_len   = k2add.size(self.k_seq_dim)
         gpu_cache_len = k_gpu.size(self.k_seq_dim) if k_gpu is not None else 0 
         # self.usecase_warn(add_len>self.start_sizes[idx],f"query length > recent window at layer {idx}")
         thresh = min(self.recent_sizes[idx]+self.start_sizes[idx],self.gpu_cache_max)
-        with torch.cuda.stream(self.copy_steam):
+        with torch.cuda.stream(self.copy_stream):
 
 
             if gpu_cache_len==0:
         
             
-                ## case 1: no previous cache, added cache smaller than allowed
+                ## case 1: no previous cache, added cache smaller than allowed on GPU
                 if add_len <= thresh: 
                     self.kv_cache[idx] = [k2add, v2add,None,None]  
                     return
                 
-                ## case 2:  no prevrious and added cache bigger than allowed, evict part to CPU
+                ## case 2:  no prevrious and added cache bigger than allowed on GPU, evict part to CPU
                 self.kv_cache[idx] = [
 
                     #####################
@@ -118,7 +120,7 @@ class KVCache_manager_:
                 
                 
             else:
-                ## case 3: previous cache + added cache smaller than allowed
+                ## case 3: previous cache + added cache smaller than allowed on GPU
                 if add_len+gpu_cache_len<= thresh:
                     
                     
@@ -148,7 +150,7 @@ class KVCache_manager_:
                         v_cpu,
                 
                     ]
-                ## case 4: previous cache + added cache bigger than allowed, added cache smaller than recent window size
+                ## case 4: previous cache + added cache bigger than allowed on GPU, added cache smaller than recent window size
                 elif add_len<  self.recent_sizes[idx] :
                     
                     evict_len = add_len - (thresh-gpu_cache_len) 
@@ -216,18 +218,26 @@ class KVCache_manager_:
         
         # to be optimized ...
         if self.cpu_kv_flags[idx]:
-            cpu_cache_len = self.kv_cache[idx][2].size(self.k_seq_dim)
-            pad_len = self.blk_size-cpu_cache_len%self.blk_size
-            self.blk_num[idx] = (cpu_cache_len+self.blk_size-1)//self.blk_size
-            if self.batched: batch_size  = self.kv_cache[idx][2].size(0)
-            if self.k_seq_dim==1:
-                blk = F.pad( self.kv_cache[idx][2], pad=(0,0,0,0,0, pad_len), value=0).reshape(batch_size,-1,self.blk_size,self.num_heads,self.head_dim)
-                    
+            k_cpu  = self.kv_cache[idx][2]
+            k_cpu_shape = k_cpu.size()
+            cache_len = k_cpu_shape[self.k_seq_dim]
+            pad_len = self.blk_size-cache_len%self.blk_size
+            self.blk_num[idx] = (cache_len+self.blk_size-1)//self.blk_size
+           
+           
+            pad_ = ([0 for _ in range((k_cpu.dim()-self.k_seq_dim-1)*2)] + [0,pad_len])
+            reshape_ = [k_cpu_shape[i] for i in range(k_cpu.dim())]
+            reshape_[self.k_seq_dim] =  self.blk_size
+            reshape_.insert(self.k_seq_dim,-1 )   
+            reshape_ = tuple(reshape_)
+            
+            blk = F.pad( k_cpu, pad=pad_, value=0).reshape(reshape_)
                 
-                self.blk_dim_min_max = [
-                    blk.min(dim=self.k_seq_dim+1)[0],
-                    blk.max(dim=self.k_seq_dim+1)[0]
-                    ] 
+            
+            self.blk_dim_min_max = [
+                blk.min(dim=self.k_seq_dim+1)[0],
+                blk.max(dim=self.k_seq_dim+1)[0]
+                ] 
                      
         return 
   
@@ -263,16 +273,22 @@ class KVCache_manager_:
 
 
 
+# def load_kv_manager(config):
+
 
 
 def test_correctness(args,log):
     gpu_cache_device = "cuda"
     num_layers=32; seq_len = args.seq_len;num_heads=32;head_dim=128; batch_size=args.batch_size
-    k_cache = torch.randn(batch_size, seq_len, num_heads,head_dim, device=gpu_cache_device).half()
-    v_cache = torch.randn(batch_size, seq_len, num_heads,head_dim, device=gpu_cache_device).half()
+    k_cache = torch.randn(batch_size,num_heads, seq_len,head_dim, device=gpu_cache_device).half()
+    v_cache = torch.randn(batch_size,num_heads, seq_len,head_dim, device=gpu_cache_device).half()
     batch_size = args.batch_size; block_size=args.block_size
-
-    KVCache_manager =  KVCache_manager_( block_size=block_size,gpu_cache_device=gpu_cache_device,num_layers=num_layers)
+    copy_stream = torch.cuda.Stream()
+    KVCache_manager =  KVCache_manager_( 
+                copy_stream=copy_stream,
+                block_size=block_size,
+                gpu_cache_device=gpu_cache_device,
+                num_layers=num_layers)
     past_key_values =  [[k_cache,v_cache] for _ in range(num_layers)]
 
 
@@ -283,13 +299,13 @@ def test_correctness(args,log):
     
     # test case 2: add kv cache
     for add_len in [1,10,50,60,100,1000]:
-        k_cache2add = torch.randn(batch_size, add_len, num_heads,head_dim, device=gpu_cache_device).half()
-        v_cache2add = torch.randn(batch_size, add_len, num_heads,head_dim, device=gpu_cache_device).half()
+        k_cache2add = torch.randn(batch_size, num_heads, add_len,head_dim, device=gpu_cache_device).half()
+        v_cache2add = torch.randn(batch_size, num_heads, add_len,head_dim, device=gpu_cache_device).half()
 
         past_key_values2add =  [[k_cache2add,v_cache2add] for _ in range(num_layers)]
          
         KVCache_manager.add_kv_cache(past_key_values2add)
-    KVCache_manager.print_gpu_coverage()
+        KVCache_manager.print_gpu_coverage(0)
        
     
     print("====================================")
@@ -297,8 +313,8 @@ def test_correctness(args,log):
     # test case 3: add kv cache by layer
     for add_len in [1,10,50,60,100,1000]:
         for idx in range(num_layers):
-            k_cache2add = torch.randn(batch_size, add_len, num_heads,head_dim, device=gpu_cache_device).half()
-            v_cache2add = torch.randn(batch_size, add_len, num_heads,head_dim, device=gpu_cache_device).half()
+            k_cache2add = torch.randn(batch_size, num_heads, add_len,head_dim, device=gpu_cache_device).half()
+            v_cache2add = torch.randn(batch_size, num_heads, add_len,head_dim, device=gpu_cache_device).half()
 
             past_key_values2add =  [ k_cache2add,v_cache2add]
             
--- beyond/attention_methods.py
+++ beyond/attention_methods.py
@@ -4,38 +4,30 @@ import argparse
 import time 
 from beyond.utils import *
  
-    
+from transformers.models.llama.modeling_llama import LlamaRotaryEmbedding,rotate_half
+
 def mha_logSum( q,k,v):
             
-        if q.dim()==4:
-            # batched config 
-            s  = k.size(1)
-            qs = q.size(1)
-            num_heads = q.size(2)
-            head_dim  = q.size(3)
-            scaling = head_dim ** -0.5
-            batch_size = k.size(0)
-            
-            q = q.permute(0, 2, 1, 3).reshape(batch_size  * num_heads, qs,  head_dim)* scaling # bh,qs,d
-            k = k.permute(0, 2, 3, 1).reshape(batch_size  * num_heads, head_dim, s) # bh,d,s
-            v = v.permute(0, 2, 1, 3).reshape(batch_size  * num_heads, s, head_dim) # bh,s,d
-        else:
-            # single query  config 
-            num_heads = q.size(1)
-            head_dim  = q.size(2)
-            scaling = head_dim ** -0.5
-            s  = k.size(0)
-            qs = q.size(0)
-            q = q.permute(1, 0, 2) * scaling # h,qs,d
-            k = k.permute(1, 2, 0) # h,d,s
-            v = v.permute(1, 0, 2) # h,s,d
-            
+        
+        #shape: b,h,s,d
+        
+        num_heads  = q.size(1)
+        head_dim   = q.size(3)
+        batch_size = q.size(0)
+        scaling = head_dim ** -0.5
+         
+        
+        q = q.reshape(batch_size  * num_heads, -1,  head_dim)* scaling # bh,qs,d
+        k = k.permute(0,1,3,2).reshape(batch_size  * num_heads, head_dim, -1) # bh,d,s
+        v = v.reshape(batch_size  * num_heads, -1, head_dim) # bh,s,d
+    
         attn_weights = torch.bmm(q,k)  
         max_scores, _ = attn_weights.max(dim=-1, keepdim=True) 
         exp_scores = torch.exp(attn_weights - max_scores) 
         sum_exp_scores = exp_scores.sum(dim=-1, keepdim=True)
         log_sum = (torch.log(sum_exp_scores)  + max_scores)*torch.tensor(1.4427) 
         attn_weights = exp_scores / sum_exp_scores 
+       
         value  = torch.bmm(attn_weights, v).permute(1,0,2) 
         if check_dtype(value,torch.float32): value = value.half() 
         if check_dtype(log_sum,torch.float16): log_sum = log_sum.float() 
@@ -46,17 +38,26 @@ def mha_logSum( q,k,v):
     
      
  
+def apply_rotary_pos_emb_single(x, cos, sin, position_ids):
+     
+    # The first two dimensions of cos and sin are always 1, so we can `squeeze` them.
+    cos = cos.squeeze(1).squeeze(0)  # [seq_len, dim]
+    sin = sin.squeeze(1).squeeze(0)  # [seq_len, dim]
+    cos = cos[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
+    sin = sin[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
+    x_embed = (x * cos) + (rotate_half(x) * sin)
+    return x_embed
+
 
 
  
 #wrapper for flashinfer merge state functions 
 class merge_state_:
-    def __init__(self,num_heads,batched=False,in_place=False):
-       self.num_heads = num_heads
-       self.batched = batched
-       self.in_place = in_place
-
-    def __call__(self, va,sa,vb,sb,batched=False):
+    def __init__(self,num_heads,in_place=False):
+        self.num_heads = num_heads
+        self.in_place = in_place
+         
+    def __call__(self, va,sa,vb,sb):
        
         assert check_tensor_device(va,'cpu') , f"va should be on CPU"
         assert check_tensor_device(sa,'cpu') , f"sa should be on CPU"
@@ -67,35 +68,42 @@ class merge_state_:
 
          
         head_dim = va.size(-1)
-        if self.in_place:
-            flashinfer.merge_state_in_place(va,sa,vb,sb)
-            v_out,s_out = vb,sb
-        else:
-            """
-            The maximum compute capability of the NVIDIA GPU is 1024 threads per block. 
-            Therefore, we partition the head and batch dimensions accordingly.
-            """
-            partitions_dim = 7680//head_dim
-            if partitions_dim<va.size(-2):
-                v_out,s_out = [],[]
-                num_partition = (va.size(-2)+partitions_dim-1)//partitions_dim
-                for i in range(num_partition):
-                    start,end = i*partitions_dim,min((i+1)*partitions_dim,va.size(-2))
-                    va_,sa_ = va[:,start:end,:].contiguous().pin_memory(),sa[:,start:end].contiguous().pin_memory()
-                    vb_,sb_ = vb[:,start:end,:].contiguous(),sb[:,start:end].contiguous()
+        
+    
+        """
+        The maximum compute capability of the NVIDIA GPU is 1024 threads per block. 
+        Therefore, we partition the head and batch dimensions accordingly.
+        """
+        partitions_dim = 7680//head_dim
+        if partitions_dim<va.size(-2):
+            v_out,s_out = [],[]
+            num_partition = (va.size(-2)+partitions_dim-1)//partitions_dim
+            for i in range(num_partition):
+                start,end = i*partitions_dim,min((i+1)*partitions_dim,va.size(-2))
+                va_,sa_ = va[:,start:end,:].contiguous().pin_memory(),sa[:,start:end].contiguous().pin_memory()
+                vb_,sb_ = vb[:,start:end,:].contiguous(),sb[:,start:end].contiguous()
+                if self.in_place:
+                    flashinfer.merge_state_in_place(va_,sa_, vb_,sb_)
+                    v_out_,s_out_ = vb_.detach(),sb_.detach()
+                    v_out.append(v_out_);s_out.append(s_out_) 
+
+                else:
                     v_out_,s_out_ =  flashinfer.merge_state(   va_,sa_, vb_,sb_ )
                     v_out.append(v_out_);s_out.append(s_out_) 
 
-                v_out,s_out = torch.cat(v_out,dim=1),torch.cat(s_out,dim=1)
-              
+            v_out,s_out = torch.cat(v_out,dim=1),torch.cat(s_out,dim=1)
+            
+        else:
+            if self.in_place:
+                flashinfer.merge_state_in_place(va_,sa_, vb_,sb_)
+                v_out,s_out = vb,sb
             else:
-                 
                 v_out,s_out = flashinfer.merge_state(  va,sa,vb,sb)
 
-        if self.batched:
-             
-            s,d = v_out.size(0),v_out.size(2)
-            v_out = v_out.reshape(s,-1,self.num_heads,d).permute(1,0,2,3) # s,b,h,d 
+        
+            
+        s,d = v_out.size(0),v_out.size(2)
+        v_out = v_out.reshape(s,-1,self.num_heads,d).permute(1,0,2,3) # s,b,h,d 
 
         # s_out shape: s,bh
         return v_out,s_out
@@ -126,51 +134,91 @@ def test_correctness(args,log):
 
         num_heads=40; hidden_size=5120
      
-
+    start  = 40
+    recent = 1024
     seq_len = args.seq_len
     head_dim = hidden_size//num_heads
     ratio = args.ratio
     partial_len = int( seq_len*ratio )
     batch_size = args.batch_size
+    q_len = args.q_len
     print(f"batch_size:{batch_size}")
      
     
     assert  (partial_len>0),  f"Partial length can't be 0 ..."
-    if batch_size==1:
-        k_cache = torch.randn(seq_len, num_heads,head_dim, device='cpu').half()
-        v_cache = torch.randn(seq_len, num_heads,head_dim, device='cpu').half()
-        q       = torch.randn(args.q_len, num_heads,head_dim, device='cuda:0').half()
-        merge_state = merge_state_(num_heads )
-      
-    else:
-        k_cache = torch.randn(batch_size,seq_len, num_heads,head_dim, device='cpu').half()
-        v_cache = torch.randn(batch_size,seq_len, num_heads,head_dim, device='cpu').half()
-        q       = torch.randn(batch_size,args.q_len, num_heads,head_dim, device='cuda:0').half()
-        merge_state = merge_state_(num_heads,batched=True)
-    
-    k_dim   = 0 if batch_size==1 else 1
+     
+    k_cache = torch.randn(batch_size,num_heads,seq_len,head_dim, device='cpu').half()
+    v_cache = torch.randn(batch_size,num_heads,seq_len,head_dim, device='cpu').half()
+    q       = torch.randn(batch_size,num_heads,q_len,head_dim, device='cuda:0').half()
+    merge_state = merge_state_(num_heads)
+     
+    k_dim   =  2
     slice = DIM_TO_SLICE[k_dim]
-
+    rotary_emb = LlamaRotaryEmbedding(head_dim)
  
+    print(  rotary_emb(v_cache, seq_len=1000000)[1][0,0,0,:])
+    print(  rotary_emb(v_cache, seq_len=1000000)[1][0,0,-1,:])
+    if args.RoPE:
+         
+        cos, sin = rotary_emb(v_cache,seq_len)
+      
+        q_ref =  apply_rotary_pos_emb_single(q.cpu(), cos, sin, torch.arange(seq_len-q_len,seq_len).unsqueeze(0))
+ 
+        k_ref = apply_rotary_pos_emb_single(k_cache, cos, sin, torch.arange(seq_len).unsqueeze(0))
+        
+    v_reference,_ = mha_logSum(q_ref.cpu() ,k_ref.cpu(),v_cache.cpu())
+     
+    v_reference = v_reference.reshape(args.q_len,-1,num_heads,head_dim).permute(1,0,2,3)
     
-    v_reference,_ = mha_logSum(q ,k_cache.cuda(),v_cache.cuda())
-    if batch_size>1:
-        v_reference = v_reference.reshape(args.q_len,-1,num_heads,head_dim).permute(1,0,2,3)
     
-    start  = 40
-    recent = 1024
-    st = time.time()
-    va,sa = mha_logSum(q.cpu() ,torch.cat([slice(k_cache,0,start),slice(k_cache,recent,seq_len)],dim=k_dim ) ,torch.cat([slice(v_cache,0,start),slice(v_cache,recent,seq_len)],dim=k_dim ))
-    print(f"CPU attention length: {partial_len}, attention time: {time.time()-st}, ")
+
+     
+    
 
     
+
+
+    q_cpu = q.cpu() 
+    k_cpu = slice(k_cache,start,seq_len-recent) 
+    v_cpu = slice(v_cache,start,seq_len-recent) 
+    
+    
+    
+    if args.RoPE:
+         
+         
+         
+        q_cpu =  apply_rotary_pos_emb_single(q_cpu, cos, sin, torch.arange(seq_len-q_len,seq_len).unsqueeze(0))
+ 
+        k_cpu = apply_rotary_pos_emb_single(k_cpu, cos, sin, torch.arange(start,seq_len-recent).unsqueeze(0))
+     
     st = time.time()
-    vb,sb = mha_logSum(q ,slice(k_cache,start,recent).cuda(),slice(v_cache,start,recent).cuda())
-    print(f"GPU attention length: {seq_len-partial_len}, attention time: {time.time()-st}, ")
+    va,sa = mha_logSum(q_cpu,k_cpu,v_cpu)
+    print(f"CPU attention length: {seq_len-start-recent}, attention time: {time.time()-st}, ")
+
+    
+    q_gpu = q
+    k_gpu = torch.cat([slice(k_cache,0,start),slice(k_cache,seq_len-recent,seq_len)],dim=k_dim  )
+    v_gpu = torch.cat([slice(v_cache,0,start),slice(v_cache,seq_len-recent,seq_len)],dim=k_dim ).cuda()
+
+    if args.RoPE:
+ 
+        q_gpu = q_cpu.cuda()
+        k_gpu = apply_rotary_pos_emb_single(k_gpu, cos, sin, torch.cat([torch.arange(0,start),torch.arange(seq_len-recent,seq_len)],dim=0 ).unsqueeze(0))
+        
+        
+        
+
+       
+ 
+     
+    st = time.time() 
+    vb,sb = mha_logSum(q_gpu ,k_gpu.cuda(),v_gpu.cuda())
+    print(f"GPU attention length: {start+recent}, attention time: {time.time()-st}, ")
 
     
     v_out,_ = merge_state( va,sa,vb,sb )
-    
+     
     acc = check_eq(v_out,v_reference)  
     assert  (acc>0.9),  f"accuracy {acc*100:.4}%, merge state fail..."
     print(f"Merge success, accuracy {acc*100:.4}%")
@@ -190,6 +238,7 @@ if __name__ == "__main__":
     parser.add_argument("--ratio", type=float, default=0.1)
     parser.add_argument("--repeat", type=int, default=10)
     parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--RoPE", type=bool, default=True )
     args = parser.parse_args()
     test_correctness(args,"")
      

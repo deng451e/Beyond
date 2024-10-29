4b6704b26415629aa1d79a1e0ab6e32db5dc44fe
deng451e <deng451e@gmail.com>
2024-10-29 10:58:43 -0500

    c3

--- run-models/run_gptNeox.py
+++ run-models/run_gptNeox.py
@@ -13,7 +13,9 @@ import sys
 from tqdm import tqdm
 from beyond.utils import *
 from beyond.loading import * 
-from beyond.models.modify_llama import modify_llama_attention
+from beyond.models.modify_gptNeox import modify_GPTNeoX_attention
+from datasets import load_dataset
+from datasets import load_dataset
 
 
 logger = logging.getLogger(__name__)
@@ -29,10 +31,12 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values,KVCache_manager
         use_cache=True,
     )
     past_key_values = outputs.past_key_values if not  enable_modify else None 
+   
     pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
     generated_ids = [pred_token_idx.item()]
     pos = 0
-    
+   
+        
     for _ in range(max_gen_len - 1):
         outputs = model(
             input_ids=pred_token_idx,
@@ -41,7 +45,7 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values,KVCache_manager
         )
         if enable_modify:
             past_key_values = None 
-            KVCache_manager.copy_stream.synchronize()
+             
 
         else:
             past_key_values = outputs.past_key_values
@@ -76,16 +80,12 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values,KVCache_manager
 
 
 @torch.no_grad()
-def inference(model, tokenizer, prompts, KVCache_manager=None, max_gen_len=1000,enable_modify=False):
+def inference(model, tokenizer, prompts, KVCache_manager=None, max_gen_len=100,enable_modify=False):
     past_key_values = None
   
-    for idx, prompt in enumerate(prompts[0:10]):
-        
+    for idx, prompt in enumerate(prompts):
+         
         prompt = "USER: " + prompt + "\n\nASSISTANT: "
-
-         
-         
-
         print("\n" + prompt, end="")
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids
         logger.info(f"================================")
@@ -98,73 +98,86 @@ def inference(model, tokenizer, prompts, KVCache_manager=None, max_gen_len=1000,
         past_key_values = greedy_generate(
             model, tokenizer, input_ids, past_key_values,KVCache_manager, max_gen_len=max_gen_len,enable_modify=enable_modify
         )
-     
+        print(f"================================")
 
 def main(args):
-    model_name_or_path = args.model_name_or_path
-
-    # load model 
-    model, tokenizer = load_model(model_name_or_path)
-    test_filepath = os.path.join(args.data_root, "mt_bench.jsonl")
+    
     
 
     # load dataset 
-    print(f"Loading data from {test_filepath} ...")
-    if not os.path.exists(test_filepath):
-        download_url("https://raw.githubusercontent.com/lm-sys/FastChat/main/fastchat/llm_judge/data/mt_bench/question.jsonl",args.data_root,)
-        os.rename(os.path.join(args.data_root, "question.jsonl"), test_filepath)
-
-    list_data = load_json(test_filepath)
-    
-    prompts = []
-    for sample in list_data:
-        prompts += sample["turns"]
- 
-
-    
-  
-     # load KV manager 
-    config = model.config 
-    KVCache_manager = KVCache_manager_(
-        start_size=10,
-        recent_size=50,
-        k_seq_dim=2,
-        v_seq_dim=2,
-        head_dim=config.hidden_size//config.num_attention_heads,
-        num_heads=config.num_attention_heads,
-        num_layers=config.num_hidden_layers,
-        gpu_cache_max=2000,
-        gpu_cache_device="cuda",
-    )
-
- 
-    if args.config_file_path:
-        KVCache_manager = set_kv_manager_config(KVCache_manager,args.config_file_path)
- 
    
+    
+    print(f"Loading data from {args.data_root} ...")
+    list_data = load_dataset(args.data_root)['train']
+    
+     
+    prompts = []
+    outputs = []
+    cnt = 100
+    for sample in list_data :
+        
+        hold =  sample['instruction']+ ': '+ sample['input'] if sample['input'] else sample['instruction']
+        prompts += [hold]
+        outputs += [sample['output']]
+         
+        cnt -= 1 
+        if cnt==0:
+            break
+ 
+    
+    ## load model 
+    print(f"Loading model from {args.model_name_or_path} ...")
+    model_name_or_path = args.model_name_or_path
+    model, tokenizer = load_model(model_name_or_path)
+    
+    config = model.config 
+    
+    # load KV manager 
+    KVCache_manager = None
 
     if args.enable_modify:
-        modify_llama_attention(model,KVCache_manager)
+         
+        config = model.config 
+        KVCache_manager = KVCache_manager_(
+            start_size=4,
+            recent_size=50,
+            k_seq_dim=2,
+            v_seq_dim=2,
+            head_dim=config.hidden_size//config.num_attention_heads,
+            num_heads=config.num_attention_heads,
+            num_layers=config.num_hidden_layers,
+            gpu_cache_max=2000,
+            cpu_attn_size=2000,
+            gpu_cache_device="cuda",
+        )
+
     
+        # if args.config_file_path:
+        #     print(f"Loading KV manager from {args.config_file_path} ...")
+        #     KVCache_manager = set_kv_manager_config(KVCache_manager,args.config_file_path)
+ 
+        KVCache_manager.print_coverage()
 
-
+        modify_GPTNeoX_attention(model,KVCache_manager)
+    
+    ## start inference 
     inference(
         model,
         tokenizer,
         prompts,
-        KVCache_manager,
+        KVCache_manager=KVCache_manager,
         enable_modify=args.enable_modify,
     )
 
 
 if __name__ == "__main__":
-    os.remove("DEBUG.log")
-    logging.basicConfig(filename='DEBUG.log', level=logging.INFO)
+    if os.path.exists("KV_cache_statics.log"): os.remove("KV_cache_statics.log")
+    logging.basicConfig(filename='KV_cache_statics.log', level=logging.INFO)
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config_file_path", type=str, default=None)
-    parser.add_argument("--model_name_or_path", type=str, default="lmsys/vicuna-13b-v1.3")
-    parser.add_argument("--data_root", type=str, default="data/")
+    parser.add_argument("--config_file_path", type=str, default="kv_manager_InitConfig/lotus-12B.json")
+    parser.add_argument("--model_name_or_path", type=str, default="hakurei/lotus-12B")
+    parser.add_argument("--data_root", type=str, default="hakurei/open-instruct-v1")
     parser.add_argument("--enable_modify", action="store_true")
     args = parser.parse_args()
     main(args)
--- run-models/run_llama.py
+++ run-models/run_llama.py
@@ -77,16 +77,12 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values,KVCache_manager
 
 
 @torch.no_grad()
-def inference(model, tokenizer, prompts, KVCache_manager=None, max_gen_len=100,enable_modify=False):
+def inference(model, tokenizer, prompts, KVCache_manager=None, max_gen_len=1000,enable_modify=False):
     past_key_values = None
   
-    for idx, prompt in enumerate(prompts[0:10]):
+    for idx, prompt in enumerate(prompts):
         
         prompt = "USER: " + prompt + "\n\nASSISTANT: "
-
-         
-         
-
         print("\n" + prompt, end="")
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids
         logger.info(f"================================")
@@ -113,41 +109,44 @@ def main(args):
         os.rename(os.path.join(args.data_root, "question.jsonl"), test_filepath)
 
     list_data = load_json(test_filepath)
-    
+    cnt = 0
     prompts = []
     for sample in list_data:
         prompts += sample["turns"]
- 
+        cnt += 1 
+    print(cnt)
 
     # load model 
     print(f"Loading model from {args.model_name_or_path} ...")
     model_name_or_path = args.model_name_or_path
     model, tokenizer = load_model(model_name_or_path)
     
-
     # load KV manager 
-    config = model.config 
-    KVCache_manager = KVCache_manager_(
-        start_size=4,
-        recent_size=1000,
-        k_seq_dim=2,
-        v_seq_dim=2,
-        head_dim=config.hidden_size//config.num_attention_heads,
-        num_heads=config.num_attention_heads,
-        num_layers=config.num_hidden_layers,
-        gpu_cache_max=2000,
-        cpu_attn_size=2000,
-        gpu_cache_device="cuda",
-    )
-
- 
-    # if args.config_file_path:
-    #     print(f"Loading KV manager from {args.config_file_path} ...")
-    #     KVCache_manager = set_kv_manager_config(KVCache_manager,args.config_file_path)
- 
-    KVCache_manager.print_coverage()
+    KVCache_manager = None
 
     if args.enable_modify:
+         
+        config = model.config 
+        KVCache_manager = KVCache_manager_(
+            start_size=4,
+            recent_size=1000,
+            k_seq_dim=2,
+            v_seq_dim=2,
+            head_dim=config.hidden_size//config.num_attention_heads,
+            num_heads=config.num_attention_heads,
+            num_layers=config.num_hidden_layers,
+            gpu_cache_max=2000,
+            cpu_attn_size=2000,
+            gpu_cache_device="cuda",
+        )
+
+    
+        if args.config_file_path:
+            print(f"Loading KV manager from {args.config_file_path} ...")
+            KVCache_manager = set_kv_manager_config(KVCache_manager,args.config_file_path)
+ 
+        KVCache_manager.print_coverage()
+
         modify_llama_attention(model,KVCache_manager)
     
     # start inference 
@@ -155,7 +154,7 @@ def main(args):
         model,
         tokenizer,
         prompts,
-        KVCache_manager,
+        KVCache_manager=KVCache_manager,
         enable_modify=args.enable_modify,
     )
 

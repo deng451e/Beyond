39edfef1b11ebe6901f0bb6c386922d1f2b50b29
deng451e <deng451e@gmail.com>
2024-11-10 12:34:49 -0600

    c3

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
 pip install transformers==4.33.0 accelerate datasets evaluate wandb scikit-learn scipy sentencepiece
 pip install cmake==3.30.4
-pip install -r requirements.txt
+ 
 
-#
+
 cd $PWD/3rparty/flashinfer/python
 pip install -e .
 

2e5e94b04e028dd1e6225d36dd593f3d6a12abbd
deng451e <deng451e@gmail.com>
2024-11-10 12:40:02 -0600

    c3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
-pip install transformers==4.33.0 accelerate datasets evaluate wandb scikit-learn scipy sentencepiece
-pip install cmake==3.30.4
+# pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
+# pip install transformers==4.33.0 accelerate datasets evaluate wandb scikit-learn scipy sentencepiece
+# pip install cmake==3.30.4
  
 
 
@@ -8,15 +8,15 @@ cd $PWD/3rparty/flashinfer/python
 pip install -e .
 echo "flashinfer installed"
 
-cd $PWD/3rparty/Infinigen/speedup
-pip install -e infinigen
-pip install -e flexgen
-echo "Infinigen installed"
+# cd $PWD/3rparty/Infinigen/speedup
+# pip install -e infinigen
+# pip install -e flexgen
+# echo "Infinigen installed"
 
-cd $PWD/3rparty/MoA
-git checkout 0.0.1
-pip install -e .
-echo "MoA installed"
+# cd $PWD/3rparty/MoA
+# git checkout 0.0.1
+# pip install -e .
+# echo "MoA installed"
 
-cd $PWD
-python setup.py develop
+# cd $PWD
+# python setup.py develop

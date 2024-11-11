0a594b7e910b51097b6b516575313a2ef816e6e5
deng451e <deng451e@gmail.com>
2024-11-11 10:42:56 -0600

    c3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
 pip install transformers==4.33.0 accelerate datasets evaluate wandb scikit-learn scipy sentencepiece
-pip install cmake==3.30.4
+pip install cmake==3.30.4 lm-eval==0.3.0 ftfy accelerate
  
 
 

740ca1f28496696290a69a8318a21585814c0433
deng451e <deng451e@gmail.com>
2024-12-07 00:13:37 -0600

    c3

---
+++ evaluation/efficiency-test/flops-test/run-test.sh
@@ -0,0 +1,34 @@
+rm  attention-achieved-flops.log
+
+for batch_size in 1 10   
+do 
+  for  q_len in  1 10  
+  do
+      for   seq_len in   100 500 1000 5000 10000 50000 1000000 500000 1000000
+        do 
+          
+              
+          
+ 
+ 
+            CMD_="--q_len $q_len --seq_len $seq_len   --batch_size $batch_size "
+          
+
+            CMD=$CMD_" --test_cpu"
+            outpt=$(python  measure_operation_intensity.py  $CMD   2>&1  )  
+            echo "$outpt" | tee -a attention-achieved-flops.log
+ 
+            CMD=$CMD_" --test_gpu"
+            outpt=$(python  measure_operation_intensity.py  $CMD    2>&1  )  
+            echo "$outpt" | tee -a attention-achieved-flops.log
+
+            CMD=$CMD_" --test_offload"
+            outpt=$(python  measure_operation_intensity.py  $CMD   2>&1  )  
+            echo "$outpt" | tee -a attention-achieved-flops.log
+
+
+
+
+      done
+  done
+done 
